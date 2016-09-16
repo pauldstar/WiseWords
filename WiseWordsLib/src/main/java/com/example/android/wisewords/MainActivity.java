39eff8598152d550a09a5569dc630614a63be7ed
Paul Ogbeiwi <paul.x.ogbeiwi@gsk.com>
2016-09-16 18:33:30 +0100

    beautified the app; two quoteList in MainActivity to conceal internet latency; load first list in SplashActivity

--- WiseWordsLib/src/main/java/com/example/android/wisewords/MainActivity.java
+++ WiseWordsLib/src/main/java/com/example/android/wisewords/MainActivity.java
@@ -26,46 +26,99 @@ import java.util.Calendar;
 
 public class MainActivity extends Activity {
 
-  private static final String MAIN_TAG = MainActivity.class.getSimpleName();
   private static TextView quoteTextTView, quoteAuthorTView;
-  private static int quoteIndex = 0;
-  private static boolean indexHasGoneFullCycle = true;
-  private static final String htmlUrlString = "http://www.quotationspage.com/random.php3";
+  private static int quoteIndex;
+  // TODO: 16/09/2016 remember that indexHasGoneFullCycle is false
+  private static boolean indexHasGoneFullCycle = false;
+  private static boolean appJustStarted;
+  public static final String htmlUrlString = "http://www.quotationspage.com/random.php3";
   private static ArrayList<Quote> quoteList = new ArrayList<>();
+  // nextQuoteList contains the next set of quotes to conceal latency due to loading web-page
+  private static ArrayList<Quote> nextQuoteList;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    appJustStarted = true;
     quoteTextTView = (TextView) findViewById(R.id.quote_text);
     quoteAuthorTView = (TextView) findViewById(R.id.quote_author);
-    android.util.Log.d("Gesture","Phone module");
+    android.util.Log.d("Gesture", "Phone module");
     // set the first quote to appear on screen
+    // first retrieve list of quoteText and quoteAuthors from SplashActivity Bundle
+    Intent intent = getIntent();
+    Bundle bundle = intent.getExtras();
+    ArrayList<String> quoteTextList = bundle.getStringArrayList("quoteTextList");
+    ArrayList<String> quoteAuthorList = bundle.getStringArrayList("quoteAuthorList");
+    // iterate through the quote texts and authors
+    Quote quote;
+    String quoteText, quoteAuthor;
+    int listSize = quoteTextList.size();
+    for (int index = 0; index < listSize; index++) {
+      quoteText = quoteTextList.get(index);
+      quoteAuthor = quoteAuthorList.get(index);
+      quote = new Quote(quoteText, quoteAuthor);
+      quoteList.add(quote);
+    }
     getNextQuote();
     // set listener on next_icon so a click will change to next quote on list
     ImageView nextIconImageView = (ImageView) findViewById(R.id.next_quote_icon);
     nextIconImageView.setOnClickListener(new View.OnClickListener() {
-      public void onClick(View v) { getNextQuote(); }
+      public void onClick(View v) {
+        getNextQuote();
+      }
     });
     // set listener on saved_quotes_icon so a click will change to list of saved quotes
     ImageView savedQuotesImageView = (ImageView) findViewById(R.id.saved_quotes_icon);
     savedQuotesImageView.setOnClickListener(new View.OnClickListener() {
-      public void onClick(View v) { showSavedQuotes(); }
+      public void onClick(View v) {
+        showSavedQuotes();
+      }
     });
     // set listener on save_quote_icon so a click save quote in database
     ImageView saveQuoteImageView = (ImageView) findViewById(R.id.save_quote_icon);
     saveQuoteImageView.setOnClickListener(new View.OnClickListener() {
-      public void onClick(View v) { saveQuote(); }
+      public void onClick(View v) {
+        saveQuote();
+      }
     });
   }
 
-  // executed when the right_arrow_icon is clicked
+  /**
+   * executed when the refresh_icon is clicked
+   */
   public void getNextQuote() {
-    QuoteAsyncTask asyncTask = new QuoteAsyncTask();
-    asyncTask.execute(htmlUrlString);
+    // set the index to point to the next item on the list
+    quoteIndex = getNextIndex();
+    // update the quoteList if necessary
+    if (quoteIndex == 0) {
+      if (indexHasGoneFullCycle) {
+        //transfer all quotes from nextQuoteList to current quoteList
+        quoteList.clear();
+        quoteList.addAll(nextQuoteList);
+      }
+      // only access the internet if we've exhausted the current quoteList
+      // update nextQuoteList in the background
+      QuoteAsyncTask quoteAsyncTask = new QuoteAsyncTask();
+      quoteAsyncTask.execute(htmlUrlString);
+      indexHasGoneFullCycle = false;
+    } // when it comes back to zero, then we reload some more quotes
+    else if (quoteIndex == 19) indexHasGoneFullCycle = true;
+    // bind quote text and author to respective text views
+    quoteTextTView.setText(quoteList.get(quoteIndex).getQuoteText());
+    quoteAuthorTView.setText(quoteList.get(quoteIndex).getQuoteAuthor());
+    Log.d("NextQuote", "QuotesSize: " + quoteList.size() + " QuoteIndex: " + quoteIndex);
   }
 
-  // executed when the right_arrow_icon is clicked
+  private int getNextIndex() {
+    if (appJustStarted) quoteIndex = 0;
+    else quoteIndex = (quoteIndex + 1) % quoteList.size();
+    return quoteIndex;
+  }
+
+  /**
+   * executed when the pinned_icon is clicked
+   */
   public void showSavedQuotes() {
     // collect all the quote texts from the quote list for the dummy data
     ArrayList<String> quoteTextList = new ArrayList<>();
@@ -86,13 +139,12 @@ public class MainActivity extends Activity {
     startActivity(intent);
   }
 
-  // save a quote from main activity
+  /**
+   * save a quote from main activity
+   */
   public void saveQuote() {
-    // quoteIndex has already been incremented, so using the previous index
-    // decrementing using modulo; so we save the quote currently on view on MainActivity
-    int previousIndex = (quoteIndex - 1 + quoteList.size()) % quoteList.size();
-    String quoteText = quoteList.get(previousIndex).getQuoteText();
-    String quoteAuthor = quoteList.get(previousIndex).getQuoteAuthor();
+    String quoteText = quoteList.get(quoteIndex).getQuoteText();
+    String quoteAuthor = quoteList.get(quoteIndex).getQuoteAuthor();
     // first check if quote already exists in database to prevent duplication
     Uri quoteUriWithTextAndAuthor = QuoteContract.QuoteEntry.
             buildQuoteUriWithTextAndAuthor(quoteText, quoteAuthor);
@@ -114,54 +166,40 @@ public class MainActivity extends Activity {
     toast.show();
   }
 
-  public static void setQuoteTextTView(String text) {
-
-  }
-
   private class QuoteAsyncTask extends AsyncTask<String, Void, Void> {
-
-    private final String ASYNC_TAG = QuoteAsyncTask.class.getSimpleName();
 
     @Override
     protected Void doInBackground(String... params) {
       // below line for debugging; allows breakpoints to be set for the background method
       /*android.os.Debug.waitForDebugger();*/
-      String htmlPageUrl = params[0];
       // only access the internet when we've exhausted the current quoteList
-      if (quoteIndex == 0 && indexHasGoneFullCycle) {
+      if (appJustStarted || indexHasGoneFullCycle) {
+        appJustStarted = false;
+        nextQuoteList = new ArrayList<>();
         try {
-          indexHasGoneFullCycle = false;
-          // get list of .quote and .author class nodes from the html document
-          Document htmlDocument = Jsoup.connect(htmlPageUrl).get();
+          Document htmlDocument = Jsoup.connect(htmlUrlString).get();
           Element htmlBodyElement = htmlDocument.body();
-          Elements quoteTextList = htmlBodyElement.select("dt.quote > a");
-          Elements quoteAuthorList = htmlBodyElement.select("dd.author > b");
+          Elements quoteTextElements = htmlBodyElement.select("dt.quote > a");
+          Elements quoteAuthorElements = htmlBodyElement.select("dd.author > b");
           // iterate through the quotes and authors
-          if (!quoteList.isEmpty()) quoteList.clear();
           String quoteText, quoteAuthor;
           Quote quote;
-          int listSize = quoteTextList.size();
+          int listSize = quoteTextElements.size();
           for (int index = 0; index < listSize; index++) {
-            quoteText = quoteTextList.get(index).ownText();
-            quoteAuthor = quoteAuthorList.get(index).text();
+            quoteText = quoteTextElements.get(index).ownText();
+            quoteAuthor = quoteAuthorElements.get(index).text();
             quote = new Quote(quoteText, quoteAuthor);
-            quoteList.add(quote);
+            nextQuoteList.add(quote);
           }
+        } catch (IOException e) {
+          e.printStackTrace();
         }
-        catch (IOException e) { e.printStackTrace(); }
       }
       return null;
     }
 
     @Override
     protected void onPostExecute(Void result) {
-      Log.d(ASYNC_TAG, "QuotesSize: " + quoteList.size() + " QuoteIndex: " + quoteIndex);
-      quoteTextTView.setText(quoteList.get(quoteIndex).getQuoteText());
-      quoteAuthorTView.setText(quoteList.get(quoteIndex).getQuoteAuthor());
-      // set the index to point to the next item on the list
-      // when it comes back to zero, then we reload some more quotes
-      if (quoteIndex == 19) indexHasGoneFullCycle = true;
-      quoteIndex = (quoteIndex + 1) % quoteList.size();
     }
   }
 }
